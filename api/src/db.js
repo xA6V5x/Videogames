9e193dcae3ffe2bddfd49db18fe84d78b456e8ac
unknown <xA6V5x>
2022-07-20 14:23:21 -0300

    trying

--- api/src/db.js
+++ api/src/db.js
@@ -28,7 +28,7 @@ let sequelize =
                  },
                  ssl: true,
             })
-          : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+          : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
                  logging: false,
                  native: false,
             });
