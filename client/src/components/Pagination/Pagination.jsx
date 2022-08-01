480312f6cd4bf97f755789538bfa1d97ccf02683
unknown <xA6V5x>
2022-08-01 14:29:11 -0300

    arreglado setpage 1

--- client/src/components/Pagination/Pagination.jsx
+++ client/src/components/Pagination/Pagination.jsx
@@ -1,34 +1,45 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { setCurrentPage } from '../../redux/actions';
 import styles from './Pagination.module.css';
 
-const Pagination = ({ 
-    gamesPerPage,
-    allGames,
-    pagination,
-    currentPage }) => {
-    const pageNumbers = [];
+const Pagination = ({ gamesPerPage, allGames, pagination, currentPage }) => {
+     const dispatch = useDispatch();
 
-  for (let i = 1; i <= Math.ceil(allGames / gamesPerPage); i++) {
-    pageNumbers.push(i);
-  }
-  return (
-    <nav>
-      <ul className={styles.crumbs}>
-        {pageNumbers &&
-          pageNumbers.map((number) => (
-            <li className={styles.number} key={number}>
-              <div
-                className={
-                  currentPage === number ? styles.crumb__active : styles.crumb
-                }
-                onClick={() => pagination(number)}
-              >
-                {number}
-              </div>
-            </li>
-          ))}
-      </ul>
-    </nav>
-  );
-}
+     function handlePagination(number) {
+          dispatch(setCurrentPage(number));
+     }
+
+     const statePage = useSelector((state) => state.currentPage);
+
+     pagination(statePage);
+
+     const pageNumbers = [];
+
+     for (let i = 1; i <= Math.ceil(allGames / gamesPerPage); i++) {
+          pageNumbers.push(i);
+     }
+
+     return (
+          <nav>
+               <ul className={styles.crumbs}>
+                    {pageNumbers &&
+                         pageNumbers.map((number) => (
+                              <li className={styles.number} key={number}>
+                                   <div
+                                        className={
+                                             currentPage === number
+                                                  ? styles.crumb__active
+                                                  : styles.crumb
+                                        }
+                                        onClick={() => handlePagination(number)}
+                                   >
+                                        {number}
+                                   </div>
+                              </li>
+                         ))}
+               </ul>
+          </nav>
+     );
+};
 
 export default Pagination;
