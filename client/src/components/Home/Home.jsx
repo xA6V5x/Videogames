795ea1d50b5912c7087c9b1e63a2fcdc1a229023
Agustin Ojeda <96439189+xA6V5x@users.noreply.github.com>
2022-09-07 00:16:45 -0500

    Update Home.jsx

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import GamesContainer from '../GamesContainer/GamesContainer';
 import NavBarHome from '../NavBarHome/NavBarHome';
+import styles from './Home.module.css'
 
 const Home = () => {
      return (
